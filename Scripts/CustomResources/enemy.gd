e1d5fe7eda5450d7898de5ae4d376cd1fe927a44
rkpham <trumpkp@gmail.com>
2023-07-08 04:11:24 -0500

    mo re stuiff

--- Scripts/CustomResources/enemy.gd
+++ Scripts/CustomResources/enemy.gd
@@ -1,7 +1,1 @@
-class_name Enemy extends Resource
-
-@export var name: String = "John"
-@export var portrait: Texture
-@export var deck: Deck
-@export var health: int = 30
-@export var max_health: int = 30
+class_name Enemy extends BattleActorData
