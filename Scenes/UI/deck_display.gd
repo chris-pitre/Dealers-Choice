db7c3b6d7e8d4135b8ab80622e9bc7e0fa219429
rkpham <trumpkp@gmail.com>
2023-07-08 02:20:22 -0500

    d e c k

--- Scenes/UI/deck_display.gd
+++ Scenes/UI/deck_display.gd
@@ -1,8 +1,15 @@
 class_name DeckDisplay extends HBoxContainer
 
-var deck: Deck
+@export var deck: Deck: set = _set_deck
+
 
 func _ready() -> void:
+	if deck:
+		_set_deck(deck)
+
+
+func _set_deck(_deck: Deck) -> void:
+	deck = _deck
 	deck.added_card.connect(_deck_added_card)
 	deck.removed_card.connect(_deck_removed_card)
 	deck.moved_card.connect(_deck_moved_card)
