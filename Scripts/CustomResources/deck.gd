b35b41eb7a4de7b8e974c6a8018cf719562a46f0
Chris Pitre <c.tophanp3@gmail.com>
2023-07-08 03:19:10 -0400

    deck

--- Scripts/CustomResources/deck.gd
+++ Scripts/CustomResources/deck.gd
@@ -5,3 +5,24 @@ signal removed_card
 signal moved_card
 
 @export var cards: Array[Card] = []
+
+func add_card(card: Card) -> void:
+	cards.append(card)
+	added_card.emit()
+
+func remove_card(card_index: int) -> Card:
+	var card = cards.pop_at(card_index)
+	removed_card.emit()
+	return card
+
+func move_card(first_index: int, second_index: int) -> void:
+	var temp = cards[first_index]
+	cards[first_index] = cards[second_index]
+	cards[second_index] = temp
+	moved_card.emit()
+	
+func shuffle_deck():
+	for i in range(cards.size()):
+		var random_index = randi() % cards.size()
+		move_card(i, random_index)
+	
