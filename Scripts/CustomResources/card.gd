d584fd96f8417c5cce72de487578cf4f5bc23645
Chris Pitre <c.tophanp3@gmail.com>
2023-07-08 03:29:48 -0400

    action enum

--- Scripts/CustomResources/card.gd
+++ Scripts/CustomResources/card.gd
@@ -1,6 +1,9 @@
 class_name Card extends Resource
 
+enum Action{Attack, Defend, Rush, Heal}
+
 @export var name: String = "TestCard"
 @export_multiline var description: String = "This is a description."
 @export var sprite: Texture
 @export var numbers: Array[int] = []
+@export var action: Action 
