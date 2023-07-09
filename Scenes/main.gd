d0b269004f0352f626cab6faec3cd923c76e93b9
Chris Pitre <c.tophanp3@gmail.com>
2023-07-09 07:54:37 -0400

    polish kielbasa

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -18,18 +18,19 @@ func _input(event):
 func _on_battle_new_battle() -> void:
 	matches_won += 1
 	$CardSelector.get_random_cards()
-	$CardSelector.show_cards()
+	await $CardSelector.show_cards()
 	active_battle.queue_free()
 	
 func _on_card_selector_selected_card(card):
-	$CardSelector.hide_cards()
 	var new_battle = await battle_scene.instantiate()
 	add_child(new_battle)
+	await $CardSelector.hide_cards()
 	active_battle = new_battle
 	active_battle.new_battle.connect(_on_battle_new_battle)
 	active_battle.lost.connect(_on_battle_lost)
 
 func _on_battle_lost():
-	active_battle.queue_free()
 	$DeathMenu.update_score()
 	$DeathMenu.visible = true
+	await $DeathMenu.show_menu()
+	active_battle.queue_free()
