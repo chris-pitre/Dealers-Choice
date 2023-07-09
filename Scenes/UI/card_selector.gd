1091074b14a6b546c0de33efc598c5d2ed79c4ab
Chris Pitre <c.tophanp3@gmail.com>
2023-07-09 06:22:38 -0400

    ACTUALLY fixed card selector
    full of features!!!

--- Scenes/UI/card_selector.gd
+++ Scenes/UI/card_selector.gd
@@ -3,6 +3,7 @@ class_name CardSelector extends Control
 
 signal selected_card(card: Card)
 
+@onready var card_button = preload("res://Scenes/UI/card_add_button.tscn")
 
 @export var card_buttons: HBoxContainer
 
@@ -22,11 +23,30 @@ func hide_cards() -> void:
 
 func get_random_cards() -> void:
 	for card_button in card_buttons.get_children():
-		card_button.set_card(GeneratorSingleton.get_random_card())
-		card_button.pressed.connect(_on_card_add_button_pressed)
+		card_button.queue_free()
+		
+	for i in range(3):
+		var new_card_button = card_button.instantiate()
+		new_card_button.set_card(GeneratorSingleton.get_random_card())
+		match i:
+			0:
+				new_card_button.pressed.connect(_on_card_add_button_0_pressed)
+			1:
+				new_card_button.pressed.connect(_on_card_add_button_1_pressed)
+			2:
+				new_card_button.pressed.connect(_on_card_add_button_2_pressed)
+		card_buttons.add_child(new_card_button)
 
+func _on_card_add_button_0_pressed() -> void:
+	select_card(0)
 
-func _on_card_add_button_pressed(idx: int) -> void:
+func _on_card_add_button_1_pressed() -> void:
+	select_card(1)
+	
+func _on_card_add_button_2_pressed() -> void:
+	select_card(2)
+
+func select_card(idx: int) -> void:
 	print("Card %d selected." % idx)
 	var indexes = [0, 1, 2]
 	indexes.erase(idx)
