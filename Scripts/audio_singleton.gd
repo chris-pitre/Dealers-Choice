53f28f08640977aaa2ad20015e5661a3cfd816bf
rkpham <trumpkp@gmail.com>
2023-07-09 03:07:22 -0500

    silencer
    put a silencer on the pistol it is Quiet now

--- Scripts/audio_singleton.gd
+++ Scripts/audio_singleton.gd
@@ -8,13 +8,12 @@ func _ready():
 	p.play()
 	
 
-func play_sfx(stream: AudioStream) -> void:
+func play_sfx(stream: AudioStream, db: int = 0) -> void:
 	var p = AudioStreamPlayer.new()
 	add_child(p)
 	p.bus = "SFX"
+	p.volume_db = db
 	p.stream = stream
 	p.play()
 	await p.finished
 	p.queue_free()
-	
-	
