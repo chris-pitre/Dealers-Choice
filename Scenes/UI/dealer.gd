86a7834d1fb3dd7ac094c665cc1d15f7e2249975
Chris Pitre <c.tophanp3@gmail.com>
2023-07-09 02:30:51 -0400

    the point of update

--- Scenes/UI/dealer.gd
+++ Scenes/UI/dealer.gd
@@ -4,22 +4,31 @@ class_name Dealer extends VBoxContainer
 signal dealer_dealt(card: Card)
 
 
-var points: int = 0
+var points: int = 20 : set = _set_points
 
 @export var top_card: CardDisplay
 @export var num_cards_left_label: Label
 @export var action_description: Label
 @export var draw_pile: Button
+@export var point_label: Label
 
 @onready var deck: Deck = Deck.new(): set = _set_deck
 
+func _set_points(new_points: int):
+	points = new_points
+	point_label.text = str(points)
+
 func _on_shuffle_pressed() -> void:
-	deck.shuffle_deck()
-	top_card.load_card(deck.cards[0])
+	if points >= 2:
+		points -= 2
+		deck.shuffle_deck()
+		top_card.load_card(deck.cards[0])
 
 func _on_swap_back_pressed() -> void:
-	deck.move_card(0, deck.size() - 1)
-	top_card.load_card(deck.cards[0])
+	if points >= 2:
+		points -= 2
+		deck.move_card(0, deck.size() - 1)
+		top_card.load_card(deck.cards[0])
 
 func _on_mark_pressed() -> void:
 	deck.cards[0].card_flags |= Card.CardFlags.Marked
@@ -30,6 +39,13 @@ func _on_deal_button_pressed() -> void:
 	if deck.cards.size() > 0:
 		top_card.load_card(deck.cards[0])
 	num_cards_left_label.text = "Num cards left: %d" % deck.size()
+	
+func _on_discard_pressed() -> void:
+	if points >= 5 and deck.size() > 1:
+		points -= 5
+		var card = deck.remove_top_card()
+		Battle.discard.add_card(card)
+		top_card.load_card(deck.cards[0])
 
 func _set_deck(_deck: Deck) -> void:
 	deck = _deck
@@ -52,4 +68,10 @@ func _on_mark_mouse_entered() -> void:
 	action_description.text = "Marks a card, making it visible in the expanded deck view. (2 points)"
 
 func _on_mark_mouse_exited() -> void:
+	action_description.text = ""
+
+func _on_discard_mouse_entered() -> void:
+	action_description.text = "Discards the top card of the deck. (5 points)"
+
+func _on_discard_mouse_exited():
 	action_description.text = ""
--- Scripts/CustomResources/card.gd
+++ Scripts/CustomResources/card.gd
@@ -37,16 +37,16 @@ func play_card(user: BattleActor, target: BattleActor):
 		AudioSingleton.play_sfx(load("res://Assets/SFX/card.wav"))
 	if action & Action.Attack:
 		#print("%s used %s for %d damage." % [user.data.name, name, numbers[0]])
-		target.damage(numbers[0])
+		await target.damage(numbers[0])
 	if action & Action.Defend:
 		#print("%s used %s for %d defense." % [user.data.name, name, numbers[0]])
-		user.defend(numbers[0])
+		await user.defend(numbers[0])
 	if action & Action.Rush:
 		#print("%s rushed." % [user.data.name])
-		user.rush()
+		await user.rush()
 	if action & Action.Heal:
 		#print("%s is healing for %d" % [user.data.name, numbers[0]])
-		user.heal(numbers[0])
+		await user.heal(numbers[0])
 	if action & Action.Custom:
 		if custom_behavior:
-			custom_behavior.action(user, target)
+			await custom_behavior.action(user, target)
