eccd552194c2478a5a71d4a8aade64d6402c0042
Chris Pitre <c.tophanp3@gmail.com>
2023-07-08 23:20:49 -0400

    boster pack!!!!!

---
+++ Scripts/CustomCardBehavior/intimidate.gd
@@ -0,0 +1,7 @@
+class_name Intimidate
+extends CustomCardBehavior
+
+func action(user: BattleActor, target: BattleActor) -> void:
+	if target.data.deck.size() > 0:
+		target.data.deck.remove_top_card()
+		target.lose_turn()
---
+++ Scripts/CustomCardBehavior/tommy.gd
@@ -0,0 +1,8 @@
+class_name Tommy
+extends CustomCardBehavior
+
+func action(user: BattleActor, target: BattleActor) -> void:
+	target.damage(2)
+	target.damage(2)
+	target.damage(2)
+	
